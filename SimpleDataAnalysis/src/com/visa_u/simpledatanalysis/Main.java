41a7cdfaf94d69b3bc3df7f17df00af3b79547f9
Feuer <afeuer@visa.com>
2016-08-03 10:04:39 -0700

    data analysis tested

--- SimpleDataAnalysis/src/com/visa_u/simpledatanalysis/Main.java
+++ SimpleDataAnalysis/src/com/visa_u/simpledatanalysis/Main.java
@@ -15,19 +15,42 @@ public class Main {
         try {
             //Create array with indicated number of elements
             int num_elements = Integer.parseInt(in);
-            int[] data = new int[num_elements];
+            if (num_elements < 1) {
+                System.out.println("Error: please enter a positive value next time.");
+                System.exit(1);
+            }
+
+            float[] data = new float[num_elements];
+            int total = 0;
+            float min_so_far = Float.MAX_VALUE;
+            float max_so_far = Float.MIN_VALUE;
 
             for (int i = 0; i < num_elements; i++) {
                 //Prompt user to enter each element, one by one, and store values in the array
-                System.out.printf("Please enter element number %d", i + 1);
+                System.out.printf("Please enter element number %d: ", i + 1);
                 String elem_str = sc.nextLine();
-                int elem = Integer.parseInt(elem_str);
+                float elem = Float.parseFloat(elem_str);
                 data[i] = elem;
+
+                //Keep track of mean/ min/ max data
+                total += elem;
+                if (elem > max_so_far) {
+                    max_so_far = elem;
+                }
+                if (elem < min_so_far) {
+                    min_so_far = elem;
+                }
+
             }
 
             //Calculate the mean and print it out
+            float mean = total / (float) num_elements;
+            System.out.printf("The mean of the data set is %.2f", mean);
 
             //Print out the min/max
+            System.out.printf(", the min is %f, and the max is %f", min_so_far, max_so_far);
+
+            System.exit(0);
 
         } catch (NumberFormatException e) {
             System.out.println("Improperly formatted number, restart and try again");
