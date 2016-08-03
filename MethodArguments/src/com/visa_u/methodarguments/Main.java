dbf3228ed0a9975ad7e60a513a84900d979f3025
Feuer <afeuer@visa.com>
2016-08-03 16:10:59 -0700

    var length args works

--- MethodArguments/src/com/visa_u/methodarguments/Main.java
+++ MethodArguments/src/com/visa_u/methodarguments/Main.java
@@ -22,6 +22,18 @@ public class Main {
         System.out.println(addUp("one", "three")); //Calls 'int addUp'
         System.out.println(addUp(2, 3.5));
         //addUp(1, "three");  ## Doesn't compile because no signature matches addUp(int, String)
+
+        //Variable length args tests
+        //System.out.println(addUp()); ## Doesn't compile because no arguments match up to multiple methods
+        System.out.println(addUp(1));
+        System.out.println(addUp(1.5));
+        System.out.println(addUp("three"));
+        System.out.println(addUp(1, 5, 19));
+        System.out.println(addUp(1.5, 6.3, 1));
+        System.out.println(addUp("one", "one", "eight"));
+        System.out.println(addUp(0, 0, 0));
+        System.out.println(addUp(0.0, 0.0));
+        System.out.println(addUp("zero", "zero", "zero"));
     }
 
     public static int addUp(int... list) {
@@ -44,11 +56,11 @@ public class Main {
 
     public static int addUp(String... list) {
         System.out.print("String addUp result: ");
-        int sum = 0;
+        int[] ints = new int[list.length];
         for (int i = 0; i < list.length; i++) {
-            sum += Main.toNum(list[i]);
+            ints[i] = Main.toNum(list[i]);
         }
-        return sum;
+        return addUp(ints);
     }
 
     public static int toNum(String str) {
