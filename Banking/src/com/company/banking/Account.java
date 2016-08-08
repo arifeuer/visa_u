105fba0caf401585114e2acde157c3e7926d5b13
Ari Feuer <ari.feuer@berkeley.edu>
2016-08-08 15:49:57 -0700

    banking works

--- Banking/src/com/company/banking/Account.java
+++ Banking/src/com/company/banking/Account.java
@@ -4,20 +4,20 @@ package com.company.banking;
  * Created by afeuer on 8/8/16.
  */
 public abstract class Account {
-    private long balance;
+    private Long balance;
     private String holderName;
 
     public Account(String holderName) {
         this.holderName = holderName;
     }
 
-    public final void transaction(String message, long amount) {
+    public final void transaction(String message, Long amount) {
         if (this.balance == null) {
             this.balance = amount;
         } else {
             this.balance += amount;
         }
-        System.out.printf("%s: %s. New balance: %d", this.holderName, message, amount);
+        System.out.printf("%s: %s. New balance: %d%n", this.holderName, message, this.balance);
     }
 
     public long getBalance() {
--- Banking/src/com/company/banking/Main.java
+++ Banking/src/com/company/banking/Main.java
@@ -3,6 +3,19 @@ package com.company.banking;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Account[] accounts = new Account[] {new MortgageAccount("Alice", 100000), new CheckingAccount("Bob", 200), new CheckingAccount("Mallory", 1000)};
+
+	    for (int i = 0; i < 3; i++) {
+	        Account account = accounts[i];
+            boolean dep_1000 = account.deposit(1000);
+            System.out.printf("dep_1000: %b; new balance: %d%n", dep_1000, account.getBalance());
+            boolean with_500 = account.withdraw(500);
+            System.out.printf("with_500: %b; new balance: %d%n", with_500, account.getBalance());
+            boolean with_1000 = account.withdraw(1000);
+            System.out.printf("with_1000: %b; new balance: %d%n", with_1000, account.getBalance());
+            boolean with_2000 = account.withdraw(2000);
+            System.out.printf("with_2000: %b; new balance: %d%n%n%n", with_2000, account.getBalance());
+
+        }
     }
 }
