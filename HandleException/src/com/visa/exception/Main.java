676eec2580ad710d8b707c9c989c70a23d5633d1
Feuer <ari.feuer@berkeley.edu>
2016-08-09 14:07:12 -0700

    initial prompt error handling

--- HandleException/src/com/visa/exception/Main.java
+++ HandleException/src/com/visa/exception/Main.java
@@ -1,20 +1,27 @@
 package com.visa.exception;
+import com.sun.media.sound.InvalidFormatException;
+
 import java.util.Scanner;
 import java.io.File;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 	    //Prompt user to enter two filenames
         boolean validFileNames = false;
         Scanner sc = new Scanner(System.in);
         String [] fileNames = new String[2];
 
         while (!validFileNames) {
-            validFileNames = Main.initialPrompt(sc, fileNames);
+            try {
+                validFileNames = Main.initialPrompt(sc, fileNames);
+            } catch (InvalidFormatException e) {
+                System.out.println(e.getMessage());
+            }
         }
 
-        File source = new File();
+        //File source = new File(fileNames[0]);
+        //File dest = new File(fileNames[1]);
 
         //Copy the first (presumed to exist) into the second (probably doesn't exist)
 
@@ -29,17 +36,16 @@ public class Main {
         //Exit after one successful copy
     }
 
-    public static boolean initialPrompt(Scanner sc, String[] fileNames) {
+    public static boolean initialPrompt(Scanner sc, String[] fileNames) throws InvalidFormatException {
         System.out.print("Please enter two filenames, separated by a space: ");
 
         //Parse out filenames
-        fileNames = sc.nextLine().split(" ");
+        fileNames = sc.nextLine().trim().split(" ");
 
-        if (fileNames.length == 2) {
-            return true;
-        } else {
-            System.out.printf("%n Invalid formatting, please try again%n");
-            return false;
+        if (fileNames.length != 2) {
+            throw new InvalidFormatException("Invalid formatting, please try again");
         }
+
+        return true;
     }
 }
