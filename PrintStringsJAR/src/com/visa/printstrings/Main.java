527665b5411b7ef59d79263aa401acc83c11aa37
Feuer <ari.feuer@berkeley.edu>
2016-08-10 11:49:01 -0700

    Jar works

--- PrintStringsJAR/src/com/visa/printstrings/Main.java
+++ PrintStringsJAR/src/com/visa/printstrings/Main.java
@@ -3,6 +3,9 @@ package com.visa.printstrings;
 public class Main {
 
     public static void main(String[] args) {
-
+        for (String str : args) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
     }
 }
